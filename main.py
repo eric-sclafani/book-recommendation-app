daf7dc425fbba22dc9490acca3d8de743883d052
eric-sclafani <eric.sclafani321@gmail.com>
2024-02-06 13:20:42 -0500

    changed src __init__ to define variables globally

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from src import init_app  # pyright: ignore
+import sqlalchemy as sa
+import sqlalchemy.orm as so
 
-app = init_app()
+from src import app, db
+from src.models import Post, User
+
+
+@app.shell_context_processor
+def make_shell_context():
+    """When 'flask shell' is called, this func registers these items to the shell"""
+    return {"sa": sa, "so": so, "db": db, "User": User, "Post": Post}
 
 
 if __name__ == "__main__":
--- src/__init__.py
+++ src/__init__.py
@@ -1,20 +1,34 @@
+from typing import Tuple
+
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
-def init_app():
+def init_flask_app() -> Flask:
     """Construct core Flask application."""
     app = Flask(__name__)
     app.config.from_object("config.Config")
+    return app
 
-    global db  # let models.py access db
+
+def init_db_migrate(app: Flask) -> Tuple[SQLAlchemy, Migrate]:
+    """Initializes and returns the database and migration class"""
     db = SQLAlchemy(app)
     migrate = Migrate(app, db)
+    return db, migrate
 
+
+def attach_dash_app(app) -> Flask:
+    """Attaches the Dash dashboard to Flask app (not 100% sure how it works though)"""
     with app.app_context():
         from . import models, routes
         from .plotlydash.dashboard import init_dashboard
 
         app = init_dashboard(app)
-        return app
+        return app  # type:ignore
+
+
+app = init_flask_app()
+db, migrate = init_db_migrate(app)
+app = attach_dash_app(app)
